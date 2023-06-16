8cd081b0fa5eea89f0ed4e7ee64bc26f93217323
c0pper <93simonster@gmail.com>
2023-06-16 09:35:09 +0200

    login and register logics

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/api/users')
 const profiles = require('./routes/api/profiles')
 const posts = require('./routes/api/posts')
-const { use } = require('passport')
 
 const app = express()
+
+//bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 //DB config
 const db = require('./config/keys').mongoURI
@@ -17,7 +22,11 @@ mongoose
     .then(() => console.log('mongodb connected'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello'))
+//Passport middleware
+app.use(passport.initialize())
+
+//Passport config
+require('./config/passport.js')(passport)
 
 //use routes
 app.use('/api/users', users)
